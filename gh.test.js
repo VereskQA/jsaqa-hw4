320a74125badafd70db3873bf907f76ad8ddac93
Veresk <ik.veresk@gmail.com>
2022-03-14 08:31:12 +0300

    v2

--- gh.test.js
+++ gh.test.js
@@ -1,84 +1,71 @@
-let firstPage;
-let secondPage;
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+});
+
+afterEach(() => {
+  page.close();
+});
 
 describe("Github page tests", () => {
   beforeEach(async () => {
-    firstPage = await browser.newPage();
-    await firstPage.goto("https://github.com/team");
-  });
-
-  afterEach(() => {
-    firstPage.close();
+    await page.goto("https://github.com/team");
   });
 
   test("The h1 header content'", async () => {
-    await firstPage.setDefaultTimeout(3000);
-    const firstLink = await firstPage.$("header div div a");
+    await page.setDefaultTimeout(3000);
+    const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await firstPage.waitForSelector("h1");
-    const title2 = await firstPage.title();
+    await page.waitForSelector("h1");
+    const title2 = await page.title();
     expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
   });
 
   test("The first link attribute", async () => {
-    await firstPage.setDefaultTimeout(3000);
-    const actual = await firstPage.$eval("a", (link) =>
-      link.getAttribute("href")
-    );
+    await page.setDefaultTimeout(3000);
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
   });
 
   test("The page contains Sign in button", async () => {
-    await firstPage.setDefaultTimeout(3000);
+    await page.setDefaultTimeout(3000);
     const btnSelector = ".btn-large-mktg.btn-mktg";
-    await firstPage.waitForSelector(btnSelector, {
+    await page.waitForSelector(btnSelector, {
       visible: true,
     });
-    const actual = await firstPage.$eval(
-      btnSelector,
-      (link) => link.textContent
-    );
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
     expect(actual).toContain("Sign up for free");
   });
 });
 
 describe("Securuty Page", () => {
   beforeEach(async () => {
-    secondPage = await browser.newPage();
-    await secondPage.goto("https://github.com/features/security");
-  });
-
-  afterEach(async () => {
-    await secondPage.close();
+    await page.goto("https://github.com/features/security");
   });
 
   test("The h1 header content", async () => {
-    await secondPage.setDefaultTimeout(3000);
-    const firstLink = await secondPage.$("header div div a");
+    await page.setDefaultTimeout(3000);
+    const firstLink = await page.$("header div div a");
     await firstLink.click();
-    await secondPage.waitForSelector("h1");
-    const title2 = await secondPage.title();
+    await page.waitForSelector("h1");
+    const title2 = await page.title();
     expect(title2).toEqual("GitHub: Where the world builds software · GitHub");
   });
 
   test("The first link attribute", async () => {
-    await secondPage.setDefaultTimeout(3000);
-    const actual = await secondPage.$eval("a", (link) =>
-      link.getAttribute("href")
-    );
+    await page.setDefaultTimeout(3000);
+    const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     expect(actual).toEqual("#start-of-content");
   });
 
   test("The page contains Download the guide button", async () => {
-    await secondPage.setDefaultTimeout(3000);
+    await page.setDefaultTimeout(3000);
     const btnSelector = ".btn-mktg.mb-3.btn-large-mktg";
-    await secondPage.waitForSelector(btnSelector, {
+    await page.waitForSelector(btnSelector, {
       visible: true,
     });
-    const actual = await secondPage.$eval(
-      btnSelector,
-      (link) => link.textContent
-    );
+    const actual = await page.$eval(btnSelector, (link) => link.textContent);
     console.log(actual);
     expect(actual).toContain("Download the guide");
   });
